7da14e9b16892f5e13b2ff0f6aa42ff5713eb2bf
Souvik <98397984+Souvik34@users.noreply.github.com>
2023-11-19 11:15:57 +0530

    Update script.js
    Full screen enabled

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const startBtn = document.querySelector('.start-btn');
+const startBtn = document.querySelector('#start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
@@ -12,3 +12,43 @@ exitBtn.onclick = () => {
     popupInfo.classList.remove('active');
     main.classList.remove('active');
 }
+
+
+// fullscreen mode
+
+document.addEventListener('DOMContentLoaded', function () {
+    var fullscreenElement = document.documentElement;
+
+    // Toggle fullscreen on button click
+    document.getElementById('start-btn').addEventListener('click', function () {
+        if (!document.fullscreenElement) {
+            fullscreenElement.requestFullscreen();
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            }
+        }
+    });
+
+    if (!document.fullscreenEnabled) {
+        document.getElementById('start-btn').style.display = 'none';
+    }
+    else {
+
+        fullscreenElement.requestFullscreen();
+
+        // warning when exiting fullscreen mode
+        document.addEventListener('fullscreenchange', function () {
+            if (!document.fullscreenElement) {
+                if (confirm('Are you sure you want to exit the Contest?')) {
+
+                    window.close();
+                }
+                else {
+
+                    fullscreenElement.requestFullscreen();
+                }
+            }
+        });
+    }
+});
